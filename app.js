3bf1df159a9e20a1749bcbc6b0de1a3557c95d04
Feauirin <83904217+Feauirin@users.noreply.github.com>
2021-05-09 12:36:58 +0300

    Add files via upload
    Обзор персонажа КЕЙЛ из игры League of Legends

---
+++ app.js
@@ -0,0 +1,9 @@
+/* 3-d */
+var ztxt = new Ztextify(".hero-text", {
+    depth: "30px",
+    layers: 8,
+    fade: true,
+    direction: "forwards",
+    event: "pointer",
+    eventRotation: "35deg"
+ });
